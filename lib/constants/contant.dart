7c4b8fffa3b446c13ff4a14625b4a03cb3faf7db
Yogesh070 <ythapa430@gmail.com>
2021-05-26 23:11:20 +0545

    constants

---
+++ lib/constants/contant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff4285F4);
+const Color kSecondaryColor = Color(0xff34A853);
+const Color kBackgroundColor = Color(0XFFF3F3F3);
+const Color kPrimaryTextColor = Color(0xff707070);
+
+const TextStyle kHeaderText = TextStyle(
+    fontSize: 18, fontWeight: FontWeight.bold, color: kPrimaryTextColor);
