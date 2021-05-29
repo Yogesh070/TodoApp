8d61d948c50b3f7fda186037a9331112bf50e19f
Yogesh070 <ythapa430@gmail.com>
2021-05-29 12:52:24 +0545

    added homecontroller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoapp/constants/contant.dart';
+import 'package:todoapp/controllers/home_controller.dart';
+import 'package:todoapp/controllers/task_data_controller.dart';
 import 'package:todoapp/screens/homescreen.dart';
 
 void main() => runApp(Homepage());
@@ -6,16 +10,26 @@ void main() => runApp(Homepage());
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        backgroundColor: Color(0xffF3F3F3),
-        primaryColor: Color(0xff4285F4),
-        textTheme: TextTheme(
-          bodyText1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => TaskData(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => HomeCrontroller(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          backgroundColor: kBackgroundColor,
+          primaryColor: kPrimaryColor,
+          textTheme: TextTheme(
+            bodyText1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
