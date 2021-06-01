9db9916af9f3d9f4a71cb13067c797d1fb7e4ed2
Yogesh070 <ythapa430@gmail.com>
2021-06-01 14:40:21 +0545

    fixed grid view

--- lib/components/task_list_gridview.dart
+++ lib/components/task_list_gridview.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todoapp/components/task_tile.dart';
+import 'package:todoapp/constants/contant.dart';
 import 'package:todoapp/controllers/task_data_controller.dart';
 import 'package:provider/provider.dart';
 
-class TaskGridView extends StatefulWidget {
-  @override
-  _TaskGridViewState createState() => _TaskGridViewState();
-}
-
-class _TaskGridViewState extends State<TaskGridView> {
+class TaskGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double mediaWidth = MediaQuery.of(context).size.width;
     return Consumer<TaskData>(
       builder: (context, taskdata, child) {
-        return GridView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: taskdata.taskCount,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: mediaWidth > 600 ? 30 : 16.0,
-            ),
-            itemBuilder: (context, index) {
-              final task = taskdata.tasks[index];
-              return TaskTile(
-                task: task,
-              );
-            });
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          width: double.infinity,
+          child: ListView(
+            children: [
+              taskdata.pinnedTaskCount > 0
+                  ? GridView.count(
+                      shrinkWrap: true,
+                      addAutomaticKeepAlives: false,
+                      crossAxisCount: 2,
+                      physics: BouncingScrollPhysics(),
+                      crossAxisSpacing: 8,
+                      children: taskdata.pinnedTask
+                          .map(
+                            (e) => TaskTile(
+                              task: e,
+                            ),
+                          )
+                          .toList())
+                  : SizedBox.shrink(),
+              taskdata.pinnedTaskCount > 0
+                  ? Divider(
+                      color: kPrimaryColor.withOpacity(0.5),
+                      thickness: 0.8,
+                    )
+                  : SizedBox.shrink(),
+              GridView.count(
+                shrinkWrap: true,
+                addAutomaticKeepAlives: false,
+                crossAxisCount: 2,
+                physics: BouncingScrollPhysics(),
+                crossAxisSpacing: 8,
+                children: taskdata.tasks
+                    .map(
+                      (e) => TaskTile(
+                        task: e,
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
